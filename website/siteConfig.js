e47abc5854ad19d117ccfbf03389f34b74f8b975
chinhtruong1 <75346411+chinhtruong1@users.noreply.github.com>
2020-12-01 19:57:20 -0600

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,8 +22,8 @@ const siteConfig = {
     //   baseUrl: '/test-site/',
 
     // Used for publishing and more
-    projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    projectName: 'Poop Car',
+    organizationName: 'Poop Car Automotive',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
