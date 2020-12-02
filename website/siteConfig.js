5fbaa7fe0799295abec88b3b75a9d2a5ae1b57db
chinhtruong1 <75346411+chinhtruong1@users.noreply.github.com>
2020-12-01 20:00:36 -0600

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,8 +12,8 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Poop Car Automotive', // Title for your website.
+    tagline: 'Better than Electric!',
     profilePhoto: 'img/profile.png',
     url: 'https://joatmon08.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
